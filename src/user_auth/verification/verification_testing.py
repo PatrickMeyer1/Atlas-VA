8ff8d3b261698ce47ee09643281d352609d767e5
BrentMRPalmer <bpalm015@uottawa.ca>
2026-04-13 12:04:02 -0400

    Added our audio

--- src/user_auth/verification/verification_testing.py
+++ src/user_auth/verification/verification_testing.py
@@ -3,13 +3,12 @@ import torchaudio
 from speechbrain.inference.speaker import EncoderClassifier
 
 # Recording DIR
-recording_dir = Path(__file__).parent.parent / "wake_word_detection" / "training" / "data" / "Original"
+recording_dir = Path(__file__).parent.parent / "wake_word_detection" / "training" / "data" / "Preprocessed"
 
 print(recording_dir)
 
 
-# classifier = EncoderClassifier.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb")
-
-# signal, fs = torchaudio.load('tests/samples/ASR/spk1_snt1.wav')
-# embeddings = classifier.encode_batch(signal)
-# print(embeddings)
+classifier = EncoderClassifier.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb")
+signal, fs = torchaudio.load(recording_dir/'Palmer-positive')
+embeddings = classifier.encode_batch(signal)
+print(embeddings)
